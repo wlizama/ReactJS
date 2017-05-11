04609aeffb0678d75f3404a9fc1ae5151c89c370
Wilder Lizama <wilderlizama@gmail.com>
2017-05-11 17:49:59 -0500

    Refs

--- public/js/main.js
+++ public/js/main.js
@@ -20857,17 +20857,15 @@ class InputBox extends React.Component {
     constructor() {
         super();
         this.state = {
-            button_label: "Agregar"
+            button_label: "Agregar",
+            salida: ""
         };
     }
 
     onClick(evt) {
-        if (this.state.button_label == "Agregar") {
-            this.setState({ button_label: 'No agregar' });
-        } else if (this.state.button_label == "No agregar") {
-            this.setState({ button_label: 'Agregar' });
-        }
+        if (this.refs.InputTask.getValor() == "") this.setState({ salida: "El campo no puede estar vacio" });else this.setState({ salida: "Ok!" });
 
+        console.log(`Valor Referencia > ${this.refs.InputTask.getValor()}`);
         console.log(`Evento onClick ${JSON.stringify(this.state)}`);
     }
 
@@ -20875,11 +20873,16 @@ class InputBox extends React.Component {
         return React.createElement(
             'div',
             { className: 'row' },
-            React.createElement(InputItem, null),
+            React.createElement(InputItem, { ref: 'InputTask' }),
             React.createElement(
                 'button',
                 { type: 'button', onClick: this.onClick.bind(this) },
                 this.state.button_label
+            ),
+            React.createElement(
+                'label',
+                null,
+                this.state.salida
             )
         );
     }
--- src/components/boxes/InputBox.jsx
+++ src/components/boxes/InputBox.jsx
@@ -6,26 +6,27 @@ class InputBox extends React.Component{
     constructor() {
         super();
         this.state = {
-            button_label : "Agregar"
+            button_label : "Agregar",
+            salida : ""
         }
     }
 
     onClick(evt){
-        if(this.state.button_label == "Agregar"){
-            this.setState({ button_label: 'No agregar' });
-        }
-        else if(this.state.button_label == "No agregar"){
-            this.setState({ button_label: 'Agregar' });
-        }
-
+        if (this.refs.InputTask.getValor() == "") 
+            this.setState({ salida : "El campo no puede estar vacio" });
+        else
+            this.setState({ salida : "Ok!" });
+        
+        console.log(`Valor Referencia > ${this.refs.InputTask.getValor()}`);
         console.log(`Evento onClick ${JSON.stringify(this.state)}`);
     }
 
     render() {
         return(
             <div className="row">
-                <InputItem />
+                <InputItem ref="InputTask" />
                 <button type="button" onClick={this.onClick.bind(this)}>{this.state.button_label}</button>
+                <label>{this.state.salida}</label>
             </div>
         );
     }
